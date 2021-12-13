68fb101d2323918e5be1674450f828c52f906f09
HENRY-KC <henrykc24@yahoo.com>
2021-12-13 17:18:23 +0100

    Fix some issues as per review

--- person.rb
+++ person.rb
@@ -3,19 +3,19 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @id = 1
+    @id = Random.rand(1...1000000)
   end
 
   attr_accessor :name, :age
   attr_reader :id, :parent_permission
 
+  def can_use_services
+    is_of_age && @parent_permission
+  end
+
   private
 
   def of_age
     @age >= 18
-  end
-
-  def can_use_services
-    is_of_age && @parent_permission
   end
 end
